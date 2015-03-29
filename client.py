77ef3d7cc07d39f48e5b7623fca3c9914487a60f
Shubahm shukla <shubhamshuklaerssss@gmail.com>
2015-03-29 16:22:14 +0530

    Made the basic program structure for push/pull but there are errors

--- client.py
+++ client.py
@@ -1,8 +1,53 @@
 import httplib
+import sys
+import getopt
+from urlparse import urlparse
+import urllib
 
-conn=httplib.HTTPSConnection("127.0.0.1:8080")
-conn.request("GET","/index.html")
-res=conn.getresponse()
-print(res.status,res.reason)
-print(res.getheaders())
-print(res.read())
+
+def pull_process(data):
+    print(data)
+
+    
+def push_process(data):
+    print(data)
+
+
+
+def process(repo,action,message,callback):
+    remote_repo_location=urlparse(repo)
+    header={"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
+    param={"path":remote_repo_location.path,"action":action,"message":message}
+    body=urllib.urlencode(param)
+    conn=httplib.HTTPSConnection(remote_repo_location.netloc)
+    conn.request("POST","",body,header)
+    response=conn.getresponse()
+    data=response.read()
+    callback(data)
+
+
+try:
+    optlist,arg_list=getopt.getopt(sys.argv[1:],"h")
+except getopt.GetoptError:
+    print("Wrong format type use -h for help")
+
+for opt,arg in optlist:
+    if opt=="-h":
+        print("Usage")
+        print("pull remote_repo")
+        print("push remote_repo")
+        sys.exit(0)
+
+if len(arg_list) != 2:
+    print("Wrong format use -h for help")
+    sys.exit(2)
+
+if arg_list[0] == "pull":
+    process(arg_list[1],arg_list[0],"pull_message",pull_process)
+elif arg_list[0] == "push":
+    process(arg_list[1],arg_list[0],"push_message",push_process)
+else:
+    print("wrong arguments use -h for help")
+    sys.exit(2)
+
+
--- request_handler.py
+++ request_handler.py
@@ -1,13 +1,26 @@
 from BaseHTTPServer import BaseHTTPRequestHandler
 import CGIHTTPServer
+import urlparse
 #The CGIHTTPServer module defines a request-handler class, 
 #interface compatible with BaseHTTPServer.BaseHTTPRequestHandler 
 #and inherits behavior from SimpleHTTPServer.SimpleHTTPRequestHandler 
 #but can also run CGI scripts.
 
 class request_handler(BaseHTTPRequestHandler):
-    def do_GET(self):
+    def do_POST(self):
+        body=self.rfile.read()
+        param=urlparse.parse_qs(body)
+        action=param["action"][0]
+        print(param["action"][0]=="pull")
+        
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
-        self.wfile.write("Hello")
+        
+        if action=="push":
+            self.wfile.write("Push")
+            print("push")
+        elif action== "pull":
+            print("pull")
+            self.wfile.write("pull")
+
