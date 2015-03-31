87c6ef2c15c3d2b045c1a84006b3b75af0b7e190
Shubahm shukla <shubhamshuklaerssss@gmail.com>
2015-03-31 19:02:39 +0530

    Seperated settings module

---
+++ server_settings.py
@@ -0,0 +1,10 @@
+def init():
+    global port
+    global num_forks
+    global num_threads_per_fork
+    global service_dir
+
+    port=8080
+    num_forks=2
+    num_threads_per_fork=2
+    service_dir="service_dir"
